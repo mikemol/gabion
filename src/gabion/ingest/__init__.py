9a6802727863c7ad1ca2f944820caf94f74c41ec
Michael Mol <mikemol@gmail.com>
2026-02-27 14:10:25 -0500

    Add ingest adapter registry seam for boundary normalization

---
+++ src/gabion/ingest/__init__.py
@@ -0,0 +1,9 @@
+from gabion.ingest.adapter_contract import LanguageAdapter, NormalizedIngestBundle, ParsedFileUnit
+from gabion.ingest.registry import resolve_adapter
+
+__all__ = [
+    "LanguageAdapter",
+    "NormalizedIngestBundle",
+    "ParsedFileUnit",
+    "resolve_adapter",
+]
